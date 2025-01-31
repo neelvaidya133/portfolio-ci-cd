5271fbb2f4632c2043f4eac2ec890f5243c9620b
neelvaidya133 <neelvaidya122@gmail.com>
2025-01-31 01:14:18 -0500

    add vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,13 @@ import "@/styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
